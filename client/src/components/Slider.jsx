37593be00a3d01ef8fc41f73c10ea377fb847a9c
Ben Kelly <benelliottkelly@gmail.com>
2024-01-21 01:26:33 +0000

    gave the home page a loading gif while waiting for api

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -8,40 +8,47 @@ export default function Slider() {
 
 
   // States
-  const [ boardGames , setBoardGames ] = useState([])
-  const [ scroll , setScroll ] = useState([])
+  const [boardGames, setBoardGames] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     async function getData() {
       const res = await boardGamesLoader()
       console.log(res)
-      const { boardgames , genres } = res
+      const { boardgames, genres } = res
       setBoardGames(boardgames)
+      setIsLoading(false)
     }
     getData()
   }, [])
 
-  useEffect(() => {
-    if (boardGames.length > 0) {
-      setScroll(true)
-    }
-  }, [boardGames])
-  
 
   return (
     <>
-      <div className='slide-container'>
-        <Slide>
-          { boardGames.length > 0 && boardGames.map((game) => {
-            return <Link className='slide-game-link' key={uuidv4} to={`/boardgames/${game.id}`}>
-                <div className='slide-image' style={{backgroundImage: `url('${game.image}')`}}>
+      {isLoading ? (
+        <div id='loading-slide' className='slide-container'>
+          <article className='loading-container'>
+            <div className='loading-title'>
+              <h2>Loading the Library<span className='loading-dots' id='dot-1'>.</span><span className='loading-dots' id='dot-2'>.</span><span className='loading-dots' id='dot-3'>.</span></h2>
+              <div>
+                <div className="dot-flashing"></div>
+              </div>
+            </div>
+          </article>
+        </div>
+      ) : (
+        <div id='loaded-slide' className='slide-container'>
+          <Slide>
+            {boardGames.length > 0 && boardGames.map((game) => {
+              return <Link className='slide-game-link' key={uuidv4} to={`/boardgames/${game.id}`}>
+                <div className='slide-image' style={{ backgroundImage: `url('${game.image}')` }}>
                 </div>
-                  <div className='slide-title'>{game.title}</div>
+                <div className='slide-title'>{game.title}</div>
               </Link>
-          })}
-          {scroll}
-        </Slide>
-      </div>
+            })}
+          </Slide>
+        </div>
+      )}
     </>
   )
 }
